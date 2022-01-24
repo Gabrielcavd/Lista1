92bc49d9fd32c248fb32a3928dbf83947b52e9b5
Gabriel Cavalcante <80011482+Gabrielcavd@users.noreply.github.com>
2022-01-24 16:55:31 -0300

    Update main.cpp

--- ponto_em_retangulo_2/src/main.cpp
+++ ponto_em_retangulo_2/src/main.cpp
@@ -19,9 +19,21 @@ using std::max;
 // Coloque aqui qualquer função auxiliar que desejar.
 
 
-int main(void)
-{
-    // TODO: Adicione aqui seu código.
+int main(void) {
+  int x1, x2, x3, y1, y2, y3;
 
+  while(cin >> std::ws >> x1 >> y1 >> x2 >> y2 >> x3 >> y3){  
+    Ponto IE {x1,y1};
+    Ponto SD {x2,y2};
+    Ponto P {x3,y3};
+
+    if(pt_in_rect(IE, SD, P) == INSIDE){
+      cout << "inside" << endl;
+    }else if(pt_in_rect(IE, SD, P) == OUTSIDE){
+      cout << "outside" << endl;
+    }else{
+      cout << "border" << endl;
+      }
+    }
     return 0;
 }
