b021ac9ecb540e5cbd8dfa1297afb39701aac7f1
Gabriel Cavalcante <80011482+Gabrielcavd@users.noreply.github.com>
2022-01-24 16:53:51 -0300

    Update main.cpp

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -7,6 +7,14 @@ const int SIZE = 5; // input size.
 int main(void)
 {
     // TODO: Adicione aqui seu código.
+    int vector_int[5];
+    int negativos = 0;
+    for(int i = 0; i<5; i++){
+    	std::cin >> vector_int[i];
+    	if(vector_int[i] < 0)
+    		negativos++;
+    }
+    std::cout << negativos <<  std::endl;
 
     return 0;
 }
