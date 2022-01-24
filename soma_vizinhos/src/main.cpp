191a3e262ab033dc7af10bcc3384003ca92b3340
Gabriel Cavalcante <80011482+Gabrielcavd@users.noreply.github.com>
2022-01-24 16:56:25 -0300

    Update main.cpp

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -8,9 +8,24 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int main( void )
-{
-    // TODO: Adicione seu código aqui. 
+int main( void ){
+  int n, m;
+  while( std::cin >> std::ws >> m >> n){
+    int soma = m;
+    if(n > 0){
+      for(int i = 1; i<n; i++){
+        m++;
+        soma += m;
+      }
+    }
+    else if (n < 0){
+      for(int i = 1; i<n*-1; i++){
+        m--;
+        soma += m;
+      }
+    }
+    std::cout << soma << endl;
+  }
 
-    return 0;
+  return 0;
 }
